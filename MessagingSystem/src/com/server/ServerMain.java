4d6458276e1b2fa7cb96592639f509fd40e9cf64
Emre <161987797+Emre-Kurubas@users.noreply.github.com>
2024-05-05 08:28:16 +0300

    Update ServerMain.java

--- MessagingSystem/src/com/server/ServerMain.java
+++ MessagingSystem/src/com/server/ServerMain.java
@@ -2,7 +2,7 @@ package com.server;
 
 public class ServerMain {
     public static void main(String[] args) {
-        int port = 5511;
+        int port = 8818;
         Server server = new Server(port);
         server.start();
     }
